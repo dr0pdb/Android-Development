eabbaf2d4b38cde83da2b44e5dd092f7d02b4430
srv-twry <saurav007tiwary@gmail.com>
2017-04-28 23:43:42 +0530

    started developers nanodegree

--- UdacityCourses/AndroidDeveloperNanodegree/DevelopingAndroidApps/ud851-Exercises/Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ UdacityCourses/AndroidDeveloperNanodegree/DevelopingAndroidApps/ud851-Exercises/Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -24,10 +24,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.android.datafrominternet.utilities.NetworkUtils;
-
-import java.net.URL;
-
 public class MainActivity extends AppCompatActivity {
 
     private EditText mSearchBoxEditText;
@@ -48,11 +44,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // TODO (2) Create a method called makeGithubSearchQuery
-    public void makeGithubSearchQuery(){
-        String githubQuery= mSearchBoxEditText.getText().toString();
-        URL githubSearchURL = NetworkUtils.buildUrl(githubQuery);
-        mUrlDisplayTextView.setText(githubQuery.toString());
-    }
     // TODO (3) Within this method, build the URL with the text from the EditText and set the built URL to the TextView
 
     @Override
@@ -65,7 +56,11 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemThatWasClickedId = item.getItemId();
         if (itemThatWasClickedId == R.id.action_search) {
-            makeGithubSearchQuery();
+            // TODO (4) Remove the Toast message when the search menu item is clicked
+            Context context = MainActivity.this;
+            String textToShow = "Search clicked";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            // TODO (5) Call makeGithubSearchQuery when the search menu item is clicked
             return true;
         }
         return super.onOptionsItemSelected(item);
--- UdacityCourses/AndroidDeveloperNanodegree/DevelopingAndroidApps/ud851-Exercises/Lesson02-GitHub-Repo-Search/T02.06-Exercise-AddPolish/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ UdacityCourses/AndroidDeveloperNanodegree/DevelopingAndroidApps/ud851-Exercises/Lesson02-GitHub-Repo-Search/T02.06-Exercise-AddPolish/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -20,9 +20,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.android.datafrominternet.utilities.NetworkUtils;
@@ -39,9 +37,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView mSearchResultsTextView;
 
     // TODO (12) Create a variable to store a reference to the error message TextView
-    TextView errorMessageTextView;
+
     // TODO (24) Create a ProgressBar variable to store a reference to the ProgressBar
-    ProgressBar progressBarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,9 +51,8 @@ public class MainActivity extends AppCompatActivity {
         mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
 
         // TODO (13) Get a reference to the error TextView using findViewById
-        errorMessageTextView = (TextView) findViewById(R.id.tv_error_message_display);
+
         // TODO (25) Get a reference to the ProgressBar using findViewById
-        progressBarView= (ProgressBar) findViewById(R.id.pb_loading_indicator);
     }
 
     /**
@@ -73,26 +69,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // TODO (14) Create a method called showJsonDataView to show the data and hide the error
-    void showJasonDataViewer(){
-        mSearchResultsTextView.setVisibility(View.VISIBLE);
-        errorMessageTextView.setVisibility(View.INVISIBLE);
-    }
 
     // TODO (15) Create a method called showErrorMessage to show the error and hide the data
-    void showErrorMessage(){
-        mSearchResultsTextView.setVisibility(View.INVISIBLE);
-        errorMessageTextView.setVisibility(View.VISIBLE);
-    }
 
     public class GithubQueryTask extends AsyncTask<URL, Void, String> {
 
         // TODO (26) Override onPreExecute to set the loading indicator to visible
-
-
-        @Override
-        protected void onPreExecute() {
-            progressBarView.setVisibility(View.VISIBLE);
-        }
 
         @Override
         protected String doInBackground(URL... params) {
@@ -109,16 +91,11 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String githubSearchResults) {
             // TODO (27) As soon as the loading is complete, hide the loading indicator
-            progressBarView.setVisibility(View.INVISIBLE);
             if (githubSearchResults != null && !githubSearchResults.equals("")) {
                 // TODO (17) Call showJsonDataView if we have valid, non-null results
                 mSearchResultsTextView.setText(githubSearchResults);
-                showJasonDataViewer();
             }
             // TODO (16) Call showErrorMessage if the result is null in onPostExecute
-            else {
-                showErrorMessage();
-            }
         }
     }
 
